de0d563b9c7e3a68883ca25039f5a10502dd896d
Arthur Borges <arthurborges.dev@gmail.com>
2021-03-29 13:10:54 -0300

    Add the first part, where users enters their names and press Enter to the program to begin

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,21 @@
 #!/usr/bin/env ruby
-puts 'Hello world!'
+
+puts "Welcome to the Ruby's Tic-Tac-Toe Game!"
+
+puts "Enter the first player's name:"
+player1 = gets.chomp
+
+puts "Enter the second players's name:"
+player2 = gets.chomp
+
+puts "#{player1} is going to play as X and #{player2} is going to play as O!"
+
+puts "Let's start! (Press ENTER)"
+
+gets
+
+system "clear"
+system "cls"
+
+board = "+---+
+
