ca9d5aca0549fbd54038f1415f703f18dbfdb98d
jmontanholi <jonunes9326@hotmail.com.br>
2021-04-01 17:42:44 -0300

    Add rspec test case for add_moves and initial_board

--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -1,13 +1,36 @@
 require './lib/board'
 
 describe Board do
-    describe "#check_winner" do
-        board = Board.new
-        it "return true if player wins" do
-            expect(board.check_winner([3, 5, 7])).to be true
-        end
-        it "return false if player don't have the numbers to win" do
-            expect(board.check_winner([3, 5, 6])).to be false
-        end
+  let(:board) {Board.new}
+  describe "#check_winner" do
+    it "return true if player wins" do
+      expect(board.check_winner([3, 5, 7])).to be true
     end
+    it "return false if player don't have the numbers to win" do
+      expect(board.check_winner([3, 5, 6])).to be false
+    end
+  end
+
+  describe "#add_moves" do
+    it "add the input number inside whole_moves array" do
+      added_move = 5
+      board.add_moves(added_move)
+      expect(board.whole_moves).to include added_move
+    end
+  end
+
+  describe "#draw_the_board" do
+    initial_box = TTY::Box.frame '+---+---+---+',
+    "| 1 | 2 | 3 |",
+    '+---+---+---+',
+    "| 4 | 5 | 6 |",
+    '+---+---+---+',
+    "| 7 | 8 | 9 |",
+    '+---+---+---+',
+    padding: 1, align: :center
+
+    it "draws a tic tac toe board" do 
+      expect(board.draw_the_board).to eql(initial_box)
+    end
+  end
 end
